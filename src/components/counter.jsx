85e01f69d56b8dbac00c69025a9a3f3d3e80132a
skylock <emanuil.copil@gmail.com>
2021-01-08 11:47:48 +0200

    dynamic class insertion

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imageUrl: "https://picsum.photos/200",
+    count: 1,
   };
 
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+
     return (
       <div>
-        <img src={this.state.imageUrl} alt="image"></img>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={classes}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
