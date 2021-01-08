5ec9aac92c1ffce2586fd247f761f1e0b2fd19f9
skylock <emanuil.copil@gmail.com>
2021-01-08 10:35:31 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      // <div className="App">
+      //   <h1>Welcome to my react app !</h1>
+      // </div>
+      <Counter></Counter>
+    );
+  }
+}
+
+export default App;
