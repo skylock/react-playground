50acdbd4f1e6e18f8dec7be137a091ac600085cc
skylock <emanuil.copil@gmail.com>
2021-01-08 11:02:24 +0200

    removed counter from root

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
