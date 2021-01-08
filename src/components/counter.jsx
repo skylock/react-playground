89aa061e129b2a1c9da521f606b8e65b42a9df8e
skylock <emanuil.copil@gmail.com>
2021-01-08 11:40:04 +0200

    add a image

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,16 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    imageUrl: "https://picsum.photos/200",
   };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
+        <img src={this.state.imageUrl} alt="image"></img>
         <span>{this.formatCount()}</span>
         <button>Increment</button>
-      </React.Fragment>
+      </div>
     );
   }
 
