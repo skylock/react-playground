f16446d8cd91779cfcd3ae5746df986e1c3bc05a
skylock <emanuil.copil@gmail.com>
2021-01-11 11:42:21 +0200

    lift up state into app component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-
 class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          Navbar
+          {this.props.countersLenght}
         </a>
       </nav>
     );
