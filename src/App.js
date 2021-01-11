ff91f26ca12b3eab4c9e5c2cb9337df36988e8ab
skylock <emanuil.copil@gmail.com>
2021-01-11 11:22:45 +0200

    update app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-import "./App.css";
 import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Counters></Counters>
-      </div>
+      <React.Fragment>
+        <NavBar />
+        <div>
+          <Counters />
+        </div>
+      </React.Fragment>
     );
   }
 }
