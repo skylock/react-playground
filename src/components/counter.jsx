57381f93a10720f4ae00bd84f21dc004ac4bdc84
skylock <emanuil.copil@gmail.com>
2021-01-14 14:08:27 +0200

    extract button text in a function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,6 +5,9 @@ const counter = (props) => {
   const [count, setCount] = useState(props.startIndex);
   const [showHello, setShowHello] = useState(true);
 
+  const getButtontext = (showHello) => {
+    return showHello ? "Hide Hello" : "Show Hello";
+  };
   useEffect(() => {
     // console.log(`render ${count}`);
     document.title = `You clicked ${count} times`;
@@ -19,7 +22,7 @@ const counter = (props) => {
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setShowHello(!showHello)}>
-        {showHello ? "Hide Hello" : "Show Hello"}
+        {getButtontext(showHello)}
       </button>
     </div>
   );
