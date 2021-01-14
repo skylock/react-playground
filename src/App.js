8f9e913eec9c76457e813a0bf8a7e71fbde0ba2e
skylock <emanuil.copil@gmail.com>
2021-01-14 09:37:27 +0200

    fixed start page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <h1>Welcome to my react app !</h1>
-      // </div>
-      <Counter></Counter>
+      <div className="App">
+        <h1>Welcome to my react app !</h1>
+      </div>
     );
   }
 }
