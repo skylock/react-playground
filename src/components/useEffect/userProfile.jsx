deb183f2d12efe3cffc5d37f298be71a08f96ec0
skylock <emanuil.copil@gmail.com>
2021-01-15 01:07:41 +0200

    update get profile on button click

--- src/components/useEffect/userProfile.jsx
+++ src/components/useEffect/userProfile.jsx
@@ -1,25 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
 const userProfile = () => {
   const [user, setUser] = useState({});
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     console.log("mounted");
     axios.get("https://randomuser.me/api/").then((res) => {
       const [userData] = res.data.results;
       setUser(userData);
+      console.log(userData);
     });
 
     return () => {
       console.log("unmounted");
     };
-  }, []);
+  }, [toggle]);
 
   return (
     <div>
       User Profile
-      <pre>{JSON.stringify(user)}</pre>
+      {user.name && (
+        <p>
+          {user.name.first} {user.name.last}
+        </p>
+      )}
+      <button onClick={() => setToggle(!toggle)}>Get other</button>
+      <div>
+        <pre>{JSON.stringify(user, undefined, 2)}</pre>
+      </div>
     </div>
   );
 };
